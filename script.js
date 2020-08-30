c401d7bd1d9c3d4820e4f760b483068831f58ecd
ctrl030 <70302284+ctrl030@users.noreply.github.com>
2020-08-30 01:01:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 
-
     let BASE_URL = "https://api.coingecko.com/api/v3";
 
     let COINS_DATA_PAGE1_ENDPOINT = "/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h";
@@ -13,11 +12,11 @@ $(document).ready(function(){
             console.log(data)
             // $("#largeIntroTextField").html
         });
-
-
+ 
 
     });
 
+    
 
 
 
