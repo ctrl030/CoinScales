3249a4fa7c6bbd11706f75b7101a2fafa4265871
ctrl030 <70302284+ctrl030@users.noreply.github.com>
2020-08-30 00:12:25 -0300

    Creating table and a "table code writer"
    Starting to build the code that will write the table. Experimenting with different parts of the functionality bit by bit.

---
+++ cellNameCreator.js
@@ -0,0 +1,24 @@
+let rankCellRow = "rankCellRow";
+let nameCellRow = "nameCellRow";
+let marketCapCellRow = "marketCapCellRow";
+let priceCellRow = "priceCellRow";
+let volumeCellRow = "volumeCellRow";
+let circulatingSupplyCellRow = "circulatingSupplyCellRow";
+let changeLast24CellRow = "changeLast24CellRow";
+let graphCellRow = "graphCellRow";
+let moreCoinOptionsCellRow = "moreCoinOptionsCellRow";
+
+
+for (y=0; y<101; y++) {
+    console.log("<td id='"+ rankCellRow + y + "'></td>");
+    console.log("<td id='"+ nameCellRow + y + "'></td>");
+    console.log("<td id='"+ marketCapCellRow + y +"'></td>");
+    console.log("<td id='"+ priceCellRow  + y +"'></td>");
+    console.log("<td id='"+ volumeCellRow + y +"'></td>");
+    console.log("<td id='"+ circulatingSupplyCellRow + y +"'></td>");
+    console.log("<td id='"+ changeLast24CellRow + y +"'></td>");
+    console.log("<td id='"+ graphCellRow + y +"'></td>");
+    console.log("<td id='"+ moreCoinOptionsCellRow + y +"'></td>");
+
+};
+
