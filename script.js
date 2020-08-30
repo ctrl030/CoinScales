1b2f588161e866d2e6aa852e39c49acefb8da82d
ctrl030 <70302284+ctrl030@users.noreply.github.com>
2020-08-30 00:57:29 -0300

    Experimenting with API

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 $(document).ready(function(){
 
 
+    let BASE_URL = "https://api.coingecko.com/api/v3";
+
+    let COINS_DATA_PAGE1_ENDPOINT = "/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h";
+
+    let coinsDataPage1url = BASE_URL + COINS_DATA_PAGE1_ENDPOINT; 
+
+    fetch(coinsDataPage1url)
+    .then(function(res){
+        res.json().then(function(data){
+            console.log(data)
+            // $("#largeIntroTextField").html
+        });
+
+
+
+    });
 
 
 
