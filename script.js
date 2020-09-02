a29579bd7022a86e1e469e654c59900d735f9978
ctrl030 <70302284+ctrl030@users.noreply.github.com>
2020-09-01 22:40:55 -0300

    3

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 $(document).ready(function () {
   let BASE_URL = "https://api.coingecko.com/api/v3";
 
-  let COINS_DATA_PAGE1_ENDPOINT =
-    "/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h";
+  let COINS_DATA_PAGE1_ENDPOINT ="/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h";
 
   let coinsDataPage1url = BASE_URL + COINS_DATA_PAGE1_ENDPOINT;
 
@@ -17,44 +16,43 @@ $(document).ready(function () {
       console.log(data);
 
       for (let i = 0; i < data.length; i++) {
-            const percentPriceChange = Number(data[i].price_change_percentage_24h / 100);
-            
-            //To change the color of the 24h price change in percent
-            let priceColor = "blackText";
-            if (percentPriceChange > 0) {
-            priceColor = "greenText";
-            } else {
-            priceColor = "redText";
-            };
-
-            // To get the right formatting for the 24h price change in percent
-            const percentPriceChangeFormatted = Number(data[i].price_change_percentage_24h / 100).toLocaleString(undefined, {
-                style: "percent",
-                minimumFractionDigits: 2,
-            });
-
-
-
-            $("#tableBody").append(
-                `
-                    <tr>
-                        <td scope="col">${i + 1}</td>
-                        <td scope="col">${data[i].name}</td>
-                        <td scope="col">${usdFormatter.format(data[i].market_cap)}</td>
-                                    
-                        <td scope="col">${usdFormatter.format(data[i].current_price)}</td>
-                        <td scope="col">${usdFormatter.format(data[i].total_volume)}</td>
-                        <td scope="col">${data[i].circulating_supply}</td>
-                                    
-                        <td class="priceChangePercentage ${priceColor}" scope="col">${percentPriceChangeFormatted}</td>
-                        <td scope="col">Graph Placeholder</td>
-                        <td scope="col">...</td>
-                    </tr>
-                `
-            );
+        const percentPriceChange = Number(data[i].price_change_percentage_24h / 100);
+              
+        //To change the color of the 24h price change in percent
+        let priceColor = "blackText";
+        if (percentPriceChange > 0) {
+        priceColor = "greenText";
+        } else {
+        priceColor = "redText";
         };
 
-      // $("#largeIntroTextField").html
+        // To get the right formatting for the 24h price change in percent
+        const percentPriceChangeFormatted = Number(data[i].price_change_percentage_24h / 100).toLocaleString(undefined, {
+          style: "percent",
+          minimumFractionDigits: 2,
+        });
+
+
+
+        $("#tableBody").append(
+          `
+            <tr>
+              <td scope="col">${i + 1}</td>
+              <td scope="col"><img style="height: 1em" src="${data[i].image}"></img>${data[i].name}</td>
+              <td scope="col">${usdFormatter.format(data[i].market_cap)}</td>
+                                        
+              <td scope="col">${usdFormatter.format(data[i].current_price)}</td>
+              <td scope="col">${usdFormatter.format(data[i].total_volume)}</td>
+              <td scope="col">${data[i].circulating_supply}</td>
+                                        
+              <td class="priceChangePercentage ${priceColor}" scope="col">${percentPriceChangeFormatted}</td>
+              <td scope="col">Graph Placeholder</td>
+              <td scope="col">...</td>
+            </tr>
+          `
+        );
+
+      };
     });
   });
 });
