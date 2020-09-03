c2ddf7a81eba8378a4fbf04235c8de94ba68b5b4
ctrl030 <70302284+ctrl030@users.noreply.github.com>
2020-09-03 16:51:34 -0300

    5

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   let BASE_URL = "https://api.coingecko.com/api/v3";
 
-  let COINS_DATA_PAGE1_ENDPOINT ="/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h";
+  let COINS_DATA_PAGE1_ENDPOINT ="/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h&sparkline=true";
 
   let coinsDataPage1url = BASE_URL + COINS_DATA_PAGE1_ENDPOINT;
 
@@ -12,6 +12,16 @@ $(document).ready(function () {
         currency: "USD",
         maximumSignificantDigits: 20,
       });
+
+
+
+      const simpleNumberFormatter = new Intl.NumberFormat("en-US", {
+        maximumSignificantDigits: 20,
+        maximumFractionDigits: 0,
+      });
+
+
+     
 
       console.log(data);
 
@@ -33,20 +43,41 @@ $(document).ready(function () {
         });
 
 
+        $("#placeholder").sparkline(data[i].sparkline_in_7d.price, {
+          type: 'line',
+          width: '165px ',
+          height: '50px ',
+          lineColor: '#edc240',          
+          fillColor: undefined,
+          lineWidth: 2,
+          spotColor: undefined,
+          minSpotColor: undefined,
+          maxSpotColor: undefined,
+          highlightSpotColor: undefined,
+          highlightLineColor: undefined
+          });
 
+
+
+console.log(data[i].sparkline_in_7d.price) 
+
+
+
+        //To generate the market data table rows and append them to the existing html table
         $("#tableBody").append(
           `
             <tr>
-              <td scope="col">${i + 1}</td>
-              <td scope="col"><img style="height: 1em;  margin-right: 5px; my" src="${data[i].image}"></img>${data[i].name}</td>
-              <td scope="col">${usdFormatter.format(data[i].market_cap)}</td>
+              <td scope="col">${i + 1}</td>              
+              <td scope="col"><img class="iconSpacing my" src="${data[i].image}"></img>${data[i].name}</td>
+              <td scope="col" class="textAllignRight">${usdFormatter.format(data[i].market_cap)}</td>
                                         
-              <td scope="col">${usdFormatter.format(data[i].current_price)}</td>
-              <td scope="col">${usdFormatter.format(data[i].total_volume)}</td>
-              <td scope="col">${data[i].circulating_supply}</td>
+              <td scope="col" class="textAllignRight">${usdFormatter.format(data[i].current_price)}</td>
+              <td scope="col" class="textAllignRight">${usdFormatter.format(data[i].total_volume)}</td>
+              
+              <td scope="col" class="textAllignRight"> ${simpleNumberFormatter.format(Math.round(data[i].circulating_supply))} ${(data[i].symbol.toUpperCase())}</td>
                                         
-              <td class="priceChangePercentage ${priceColor}" scope="col">${percentPriceChangeFormatted}</td>
-              <td scope="col">Graph Placeholder</td>
+              <td class="priceChangePercentage ${priceColor} textAllignRight" scope="col">${percentPriceChangeFormatted}</td>
+              <td scope="col" id="placeholder"> Graph Placeholder  </td>
               <td scope="col">...</td>
             </tr>
           `
