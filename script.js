317a09b3d82f68fc04c43b7b843a947b21bc1daf
ctrl030 <70302284+ctrl030@users.noreply.github.com>
2020-09-16 02:47:17 -0300

    22

--- script.js
+++ script.js
@@ -2,6 +2,24 @@ $(document).ready(function () {
   let pageCounter = 1;
   
   let BASE_URL = "https://api.coingecko.com/api/v3";
+
+  let FIRST_ROW_Cryptocurrencies_ENDPOINT = "/coins/list";
+  let FIRST_ROW_Markets_ENDPOINT = "";
+  let FIRST_ROW_MarketCap_ENDPOINT = "";
+  let FIRST_ROW_24hVol_ENDPOINT = "";
+  let FIRST_ROW_BTCDominance_ENDPOINT = "";
+  let FIRST_ROW__ENDPOINT = "";
+
+  let firstRowAmountOfCryptoCurrenciesUrl = BASE_URL + FIRST_ROW_Cryptocurrencies_ENDPOINT;
+  fetch(firstRowAmountOfCryptoCurrenciesUrl).then(function (amountOfCryptosObject) { 
+    amountOfCryptosObject.json().then(function (amountOfCryptosOutput) { 
+    console.log("amountOfCryptosOutput.length: " + amountOfCryptosOutput.length);
+    $("#firstRowCryptocurrencies").html("Cryptocurrencies: " + "<a href=''>" + $(amountOfCryptosOutput).length + "</a>"); 
+    });
+  });
+
+
+
   let COINS_DATA_PAGE_ENDPOINT ="/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=" + pageCounter + "&sparkline=false&price_change_percentage=24h&sparkline=true";
   
   let pageToFetch = BASE_URL + COINS_DATA_PAGE_ENDPOINT;
@@ -18,78 +36,57 @@ $(document).ready(function () {
 
   tableBuildingFunction();
 
-   
-  function takeOutClickedBtnBrdr()  {
-    $("").css("box-shadow", "0 0 0 0", "rgba (0, 0, 0, 0)");
-  };
- 
-  function calltakeOutClickedBtnBrdr()  {
-    setTimeout (takeOutClickedBtnBrdr, 600);          
-  };
-
-
-  function nextButtonFunct () {
+  $(".nextPageButton, .lastPageButton").click(function() {
     
-    $(this).css("box-shadow", "0 0 0 0.2rem", "rgba (0,123,255,.5)");
+    $(this).css("box-shadow", "0 0 0 0.2rem", "rgba (0,123,255,.1)");    
+
     var myArray = Object.values($(this));
     console.log("button clicked from " + myArray);  
-    pageCounter += 1;
-     
-    if (pageCounter >1) {
-      $(".lastPageButton").show();
-    }
-    
-    if (pageCounter >=10) {
-      pageCounter = 10;
-      $(".nextPageButton").hide();
-    }
-    
-    loadCorrectPage();    
-    tableBuildingFunction();
+
+
+    if ($(this).hasClass("nextPageButton") == true) {
+      pageCounter += 1;
+    } else if ($(this).hasClass("lastPageButton") == true) {
+      pageCounter -= 1;
+    };
  
-    calltakeOutClickedBtnBrdr();
-
-    $("#largeIntroTextField").html(`Top Cryptocurrencies by Market Cap Page ${pageCounter}`);
-  };
-
-  function lastButtonFunct () {
-        
-    $(this).css("box-shadow", "0 0 0 0.2rem", "rgba (0,123,255,.5)");
-
-    pageCounter -= 1;
-
-    if (pageCounter <10) {
-      $(".nextPageButton").show();
-      }
     if (pageCounter <= 1) {
       pageCounter = 1;
-      $(".lastPageButton").hide();
-    }
+      $(".lastPageButton").hide();      
+    };
+
+    if (pageCounter > 1) {
+      $(".lastPageButton").show();
+    };
     
-    loadCorrectPage(); 
+    if (pageCounter < 10) {
+        $(".nextPageButton").show();
+    };
+
+    if (pageCounter >= 10) {
+      pageCounter = 10;
+      $(".nextPageButton").hide();
+    };
+    
+    
+    loadCorrectPage();    
+
     tableBuildingFunction();
 
-    calltakeOutClickedBtnBrdr();
-
-    $("#largeIntroTextField").html(`Top Cryptocurrencies by Market Cap Page ${pageCounter}`); 
+    let clickedButtonBeforeBrdrRmvd = 0;
+    clickedButtonBeforeBrdrRmvd = $(this);
+    setTimeout (function(){clickedButtonBeforeBrdrRmvd.css("box-shadow", "0 0 0 0", "rgba (0, 0, 0, 0)")}, 1200);
+  
+    $("#largeIntroTextField").html(`Top Cryptocurrencies by Market Cap Page ${pageCounter}`);
 
     if (pageCounter == 1) {
       $("#largeIntroTextField").html(`Top 100 Cryptocurrencies by Market Cap`);
     } 
-        
-  };
 
-  $(".nextPageButton").click(function(){
-    nextButtonFunct () 
-  });
-
-  $(".lastPageButton").click(function(){
-    lastButtonFunct () 
   });
   
   function tableBuildingFunction () {
-    loadCorrectPage();
-    
+    loadCorrectPage();    
 
     fetch(pageToFetch).then(function (res) {
       pageToFetch = BASE_URL + COINS_DATA_PAGE_ENDPOINT;      
@@ -139,8 +136,8 @@ $(document).ready(function () {
                 <td scope="col" class="lessPaddingforRow align-middle"><img class="iconSpacing my" src="${data[i].image}"></img><b>${data[i].name}</b></td>
                 <td scope="col" class="textAllignRight lessPaddingforRow align-middle">${usdFormatter.format(data[i].market_cap)}</td>
                                           
-                <td scope="col" class="textAllignRight lessPaddingforRow align-middle">${usdFormatter.format(data[i].current_price)}</td>
-                <td scope="col" class="textAllignRight lessPaddingforRow align-middle">${usdFormatter.format(data[i].total_volume)}</td>
+                <td scope="col" class="textAllignRight lessPaddingforRow align-middle"><a href="">${usdFormatter.format(data[i].current_price)}</a></td>
+                <td scope="col" class="textAllignRight lessPaddingforRow align-middle"><a href="">${usdFormatter.format(data[i].total_volume)}</a></td>
                 
                 <td scope="col" class="textAllignRight lessPaddingforRow align-middle"> ${simpleNumberFormatter.format(Math.round(data[i].circulating_supply))} ${(data[i].symbol.toUpperCase())}</td>
                                           
